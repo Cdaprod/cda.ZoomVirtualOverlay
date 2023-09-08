bfb2fd65c2f42c07c23d3530048183abf9ae290f
David <104325852+Cdaprod@users.noreply.github.com>
2023-09-08 15:46:06 -0400

    Refactor main.py for capturing screengrab and camera feed

--- main.py
+++ main.py
@@ -17,12 +17,42 @@ cam = cv2.VideoCapture(0)
 # Initialize virtual camera
 with pyvirtualcam.Camera(width=width, height=height, fps=20) as virtual_cam:
     while True:
-        # ... [rest of the code remains unchanged]
-        
+        # Capture the screengrab
+        screengrab = pygame.surfarray.array3d(pygame.display.get_surface())
+
+        # Capture the camera feed
+        ret, frame = cam.read()
+
+        # Resize the camera feed to match the screengrab dimensions
+        frame = cv2.resize(frame, (width, height))
+
+        # Overlay the camera feed on the screengrab
+        overlay = cv2.addWeighted(frame, 0.5, screengrab, 0.5, 0)
+
+        # Convert the overlayed image to RGB format for PyGame
+        overlay_rgb = cv2.cvtColor(overlay, cv2.COLOR_BGR2RGB)
+
+        # Display the overlayed image
+        pygame.surfarray.blit_array(screen, overlay_rgb)
+        pygame.display.flip()
+
         # Send the overlayed image to the virtual camera
         virtual_cam.send(overlay_rgb)
-        
-        # Sleep for a short duration before capturing the next frame
-        virtual_cam.sleep_until_next_frame()
 
-        # ... [rest of the code remains unchanged]
+        # Handle keyboard events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                # Quit the program if the window is closed
+                cam.release()
+                cv2.destroyAllWindows()
+                pygame.quit()
+                exit()
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_q:
+                    # Quit the program if 'q' is pressed
+                    cam.release()
+                    cv2.destroyAllWindows()
+                    pygame.quit()
+                    exit()
+
